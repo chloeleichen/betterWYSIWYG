30a410227a0cf94d6ddd941302c440975ccfdeae
Chloe Chen <chloeleichen@gmail.com>
2015-05-15 17:22:14 +1000

    more buttons

--- www/src/js/watchEvent.js
+++ www/src/js/watchEvent.js
@@ -1,28 +1,24 @@
+// Helpful links
+//
 // https://dvcs.w3.org/hg/editing/raw-file/tip/editing.html
+// https://newcircle.com/s/post/1096/using_the_html5_attribute_contenteditable_to_create_a_wysiwyg
+
 define([
 	'./src/js/editing/editor.js'
 	], function (editor) {
 		//Native commands that does not require value
-		var commandsSimple = ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript',
-                    'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull',
-                    'formatBlock', 'removeFormat'];
+		var doCommand = function(){
+			var buttonContainer = document.getElementById("button-container");
+			var buttons = buttonContainer.querySelectorAll('button');
 
-		var doCommand = function(sCmd){
-			var target = document.getElementById(sCmd);
-
-			if(target != null){
-				document.getElementById(sCmd).addEventListener("click", function(e){
-				editor(sCmd);
-				});	
-			}else return;
-		};
-
-
-		var watchEvent = function(){
-			for (var i = 0, len = commandsSimple.length; i < len; i ++ ){
-				doCommand(commandsSimple[i]);
+			for(var i =0, l = buttons.length; i <l; i++){
+				buttons[i].addEventListener('click', function(e){
+					e.preventDefault();
+					var command = this.dataset.event;
+					editor(command);
+				});
 			}
 		}
 
-		return watchEvent;
+		return doCommand;
 });
